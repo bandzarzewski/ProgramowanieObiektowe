99746d963ca62cb79e455e153ced231701ddf231
RENT-Komputer\RENT <m.bandzarzewski@gmail.com>
2017-08-22 21:12:27 +0200

    Dziedziczenie klasa ShapeAndCircleAndRectangleAndSquare // - dopisać testy

---
+++ src/ShapeAndCircleAndRectangleAndSquare/Shape.java
@@ -0,0 +1,48 @@
+package ShapeAndCircleAndRectangleAndSquare;
+
+/**
+ * Created by RENT on 2017-08-22.
+ */
+public class Shape {
+    private String _color = "red";
+    private boolean _filled = true;
+
+    public Shape() {
+    }
+
+    public Shape(String color, boolean filled) {
+        this._color = color;
+        this._filled = filled;
+    }
+
+    public String getColor() {
+        return _color;
+    }
+
+    public void setColor(String _color) {
+        this._color = _color;
+    }
+
+    public boolean isFilled() {
+        return _filled;
+    }
+
+    public void setFilled(boolean filled) {
+        this._filled = filled;
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("A Shape with color of ")
+                .append(_color)
+                .append(" and ");
+        if (_filled) {
+            sb.append("filled");
+        } else {
+            sb.append("not filled");
+        }
+        return sb.toString();
+
+
+    }
+}
