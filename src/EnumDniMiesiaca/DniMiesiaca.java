78c57c7b62d32758d4b2599bae57c628ac0f45f0
RENT-Komputer\RENT <m.bandzarzewski@gmail.com>
2017-08-24 20:57:50 +0200

    Poprawić kod

--- src/EnumDniMiesiaca/DniMiesiaca.java
+++ src/EnumDniMiesiaca/DniMiesiaca.java
@@ -4,4 +4,32 @@ package EnumDniMiesiaca;
  * Created by RENT on 2017-08-24.
  */
 public class DniMiesiaca {
+    public enum DniMies {
+        STYCZEN(LUTY) {
+            public DniMiesiaca next() {
+                return LUTY;
+            }
+        },
+        LUTY(STYCZEN) {
+            public DniMiesiaca next() {
+                return STYCZEN;
+            }
+        },
+        ;
+
+        public final String name;
+
+        DniMies(String name) {
+            this.name = name;
+        }
+
+        public abstract DniMiesiaca next();
+    }
+
+    public static void main(String[] args) {
+        for (DniMies dnimiesiaca : DniMies.values()) {
+            System.out.printf("%s nastepny miesiac %s", dnimiesiaca, dnimiesiaca.next());
+        }
+    }
+
 }
