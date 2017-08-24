5faa489ff22a6be9fd3b272a2a33bb6522e312a4
DESKTOP-231T7BN\Maciek <m.bandzarzewski>
2017-08-24 16:59:02 +0200

    Add Test MyCirle

--- src/MyPointMyCircle/Main.java
+++ src/MyPointMyCircle/Main.java
@@ -5,8 +5,9 @@ package MyPointMyCircle;
  */
 public class Main {
     public static void main(String[] args) {
-        MyPoint myPoint = new MyPoint(10, 10);
-        System.out.println(myPoint.calculatorDistance(5, 5));
-
+//        MyPoint myPoint = new MyPoint(10, 10);
+//        System.out.println(myPoint.calculatorDistance(5, 5));
+        MyCircle myCircle = new MyCircle();
+        System.out.println(myCircle.getCenter());
     }
 }
--- src/MyPointMyCircle/MyCircleTest.java
+++ src/MyPointMyCircle/MyCircleTest.java
@@ -1,15 +1,69 @@
-//package MyPointMyCircle;
-//
-//import org.junit.Test;
-//
-///**
-// * Created by Maciek on 23.08.2017.
-// */
-//public class MyCircleTest {
-//    @Test
-//    public void() {
-//    }
-//
-//    ;
-//
-//}
+package MyPointMyCircle;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Maciek on 23.08.2017.
+ */
+public class MyCircleTest {
+    @Test
+    public void MyCircleGetSetRadiusTest() {
+        MyCircle myCircle = new MyCircle();
+        myCircle.setRadius(2);
+        Assert.assertEquals(2, myCircle.getRadius());
+    }
+
+    @Test
+    public void MyCircleMyPointGetCenterTest() {
+        MyCircle myCircle = new MyCircle();
+        Assert.assertEquals(new MyPoint(0, 0), myCircle.getCenter());
+    }
+
+    @Test
+    public void MyCircleSetCenter() {
+        MyCircle myCircle = new MyCircle();
+        myCircle.setCenter(new MyPoint(1, 1));
+        Assert.assertEquals(new MyPoint(1, 1), myCircle.getCenter());
+    }
+
+    @Test
+    public void MyCircleGetCenterX() {
+        MyCircle myCircle = new MyCircle();
+        Assert.assertEquals(0, myCircle.getCenterX());
+    }
+
+    @Test
+    public void MyCircleSetCenterX() {
+        MyCircle myCircle = new MyCircle();
+        myCircle.setCenterX(5);
+        Assert.assertEquals(5, myCircle.getCenterX());
+    }
+
+    @Test
+    public void MyCircleGetCenterXY() {
+        MyCircle myCircle = new MyCircle();
+        Assert.assertArrayEquals(new int[]{0, 0}, myCircle.getCenterXY());
+    }
+
+    @Test
+    public void MyCircleSetCenterXY() {
+        MyCircle myCircle = new MyCircle();
+        myCircle.setCenterXY(5, 5);
+        Assert.assertArrayEquals(new int[]{5, 5}, myCircle.getCenterXY());
+    }
+
+    @Test
+    public void MyCircleGetArea() {
+        MyCircle myCircle = new MyCircle();
+        Assert.assertEquals(3.14, myCircle.getArea(), 0.01);
+    }
+
+    @Test
+    public void MyCircleCircumferenceTest() {
+        MyCircle myCircle = new MyCircle();
+        Assert.assertEquals(6.28, myCircle.getCircumference(), 0.01);
+    }
+
+
+}
