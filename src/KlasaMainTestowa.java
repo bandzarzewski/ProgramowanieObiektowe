bd3899a472c8f59d8f7ff797ec69c13655b4ab86
DESKTOP-231T7BN\Maciek <m.bandzarzewski>
2017-09-05 13:59:46 +0200

    Praca domowa

--- src/KlasaMainTestowa.java
+++ src/KlasaMainTestowa.java
@@ -1,17 +1,34 @@
-import java.util.Random;
+import java.util.Scanner;
 
 public class KlasaMainTestowa {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Wprowadz wielkosc tablicy");
+        int n = sc.nextInt();
+        int[] tab = new int[n];
 
-
-        Random random = new Random();
-        double[] analogSignal = random.doubles(8, 0.0, 2.0).toArray();
-        for (int i = 0; i < analogSignal.length; i++) {
-            if (analogSignal[i] > 1) {
-                System.out.println(analogSignal[i]);
-            }
-
-
+        System.out.println("Wprowadz elementy tablicy");
+        for (int i = 0; i < tab.length; i++) {
+            tab[i] = sc.nextInt();
         }
+        int temp = 0;
+        int numOfSwaps = 0;
+        for (int j = 0; j < tab.length; j++) {
+            for (int k = 0; k < tab.length - 1; k++) {
+                if (tab[k] > tab[k + 1]) {
+                    temp = tab[k];
+                    tab[k] = tab[k + 1];
+                    tab[k + 1] = temp;
+                    numOfSwaps++;
+                }
+            }
+        }
+//        for (int j = 0; j < tab.length; j++) {
+//            System.out.print(tab[j] + " ");
+//        }
+        System.out.println();
+        System.out.println("Array is sorted in " + numOfSwaps + " swaps");
+        System.out.println("First Element: " + tab[0]);
+        System.out.println("Last Element : " + tab[tab.length - 1]);
     }
 }
--- src/SimpleThread_Watki/MyRun.java
+++ src/SimpleThread_Watki/MyRun.java
@@ -1,15 +1,15 @@
-package SimpleThread_Watki;
-
-
-public class SimpleThread implements Runnable {
-    private int _id;
-
-    public SimpleThread(int id) {
-        this._id = id;
-    }
-
-    @Override
-    public void run() {
-
-    }
-}
+//package SimpleThread_Watki;
+//
+//
+//public class SimpleThread implements Runnable {
+//    private int _id;
+//
+//    public SimpleThread(int id) {
+//        this._id = id;
+//    }
+//
+//    @Override
+//    public void run() {
+//
+//    }
+//}
