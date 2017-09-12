6006b2b5d2c85cd664b521c6276757e61a46234e
RENT-Komputer\RENT <m.bandzarzewski@gmail.com>
2017-09-12 20:56:59 +0200

    Akutalizacja zad 3 i zad 4

--- src/Pow0709/Zad3/Main.java
+++ src/Pow0709/Zad3/Main.java
@@ -18,24 +18,30 @@ import java.util.Scanner;
 //    }
 //}
 
-
+// UZYJ LICZNIKA
 public class Main {
     public static void main(String[] args) {
         Random random = new Random();
         Scanner sc = new Scanner(System.in);
-
         System.out.println("Wprowadz liczbe: ");
         int liczba = sc.nextInt();
-
+        int licznik = 0;
+        int licznik2 = 0;
         for (int i = 0; i < liczba; i++) {
             int nowaLiczba = random.nextInt(201) - 100; // zakres od -100 do 100
-
             if (nowaLiczba > 0) {
-                System.out.println("Liczba dodatnia: " + nowaLiczba);
+                licznik++;
+                System.out.println(nowaLiczba);
             } else if (nowaLiczba < 0) {
-                System.out.println("Liczba ujemna: " + nowaLiczba);
+                licznik2++;
+                System.out.println(nowaLiczba);
             }
         }
+        System.out.println("Liczba dodatnich czyfr : " + licznik);
+        System.out.println("Liczba ujemnych czyfr : " + licznik2);
+        double a = ((double) licznik / (double) licznik2);
+        System.out.println("Stosunek liczb dodatnich do ujemnych:  " + a);
+
     }
 }
 
--- src/Pow0709/Zad4/Main.java
+++ src/Pow0709/Zad4/Main.java
@@ -4,4 +4,7 @@ package Pow0709.Zad4;
  * Created by RENT on 2017-09-07.
  */
 public class Main {
+    public static void main(String[] args) {
+
+    }
 }
